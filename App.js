b5788ff3a7c7a6bca44e8a482214d17b759b78c0
chebihiF <chebihi.f@ucd.ac.ma>
2022-04-15 11:26:37 +0000

    final commit

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, Image, Button, Alert, StatusBar } from 'react-native';
+import { Platform, SafeAreaView } from 'react-native';
+import { TouchableWithoutFeedback, TouchableOpacity, TouchableHighlight } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text 
+        numberOfLines={2} 
+        onPress={()=>console.log("Text pressed")}>
+        Hello React Native android Hello 
+        React Native android Hello React Native 
+        android Hello React Native android Hello</Text>
+      <Image style={styles.imageStyle} source={require("./assets/favicon.png")} />
+        
+      <TouchableOpacity 
+        onPress={()=>console.log("image pressed")}>
+        <Image
+          fadeDuration={2000}
+          source={
+            {
+              uri: "https://picsum.photos/200",
+              width:200,
+              height:200
+            }
+        }/>
+      </TouchableOpacity>
+       
+      <Button 
+        title='Click Me' 
+        color='orange' 
+        onPress={()=>Alert.alert('My title', 'My Message', [
+          {text: 'Yes', onPress: () => console.log("Yes")},
+          {text: 'No', onPress: ()=>console.log("No")},
+        ])} />
+
+   
+    </SafeAreaView>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  imageStyle: {
+    width: 100,
+    height: 100
+  }
 });
